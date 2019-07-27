6620d7fdb8eae4dfaaf00bfcb066d44995c8835c
Nikhil Mulchandani <nikmul19@gmail.com>
2019-07-27 10:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const FACTS=[
         'Earth के तुलना से Mars में सूरज का size तक़रीबन आधा हैं',
         'सारे ग्रहों में Jupiter का दिन सबसे कम हैं',
         'सूरज का shape एकदम गेंद आकार में हैं'
+        
+        
+        /*'A year on Mercury is just 88 days long.',
+        'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
+        'On Mars, the Sun appears about half the size as it does on Earth.',
+        'Jupiter has the shortest day of all the planets.',
+        'The Sun is an almost perfect sphere.',*/
     
     ]
 
